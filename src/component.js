1b3e1e8ecda5d461f0e303ab6b1219e917e49eb2
harshsinghdev <harshsingh.bharat@gmail.com>
2022-07-20 21:49:49 +0530

    Jai Shree Ram!

---
+++ src/component.js
@@ -0,0 +1,85 @@
+import { deepFreeze, isFn } from "./util.js";
+
+import { patch } from "./vdom.js";
+
+export default class Component extends HTMLElement {
+
+    props = {}
+
+    state = null
+
+    #frameRequest = null
+
+    #oldTree = null
+
+    constructor() {
+        super();
+
+        this.rootNode = this;
+    }
+
+    setState(newState) {
+        const nextState = isFn(newState) ? newState(this.state) : newState;
+
+        this.state = deepFreeze(nextState);
+        
+        this.#requestUpdate();
+    }
+
+    #update = () => {
+        const attributes = Array.from(this.attributes);
+
+        for(const { name, value } of attributes) {
+            this.#updateProp(name, value)
+        }
+
+        const newTree = this.render(this.state, this.props);
+
+        patch(this.rootNode, this.#oldTree, newTree);
+
+        this.#oldTree = newTree;
+    }
+
+    #requestUpdate() {
+        if(this.#frameRequest) {
+            cancelAnimationFrame(this.#frameRequest);
+        }
+
+        this.#frameRequest = requestAnimationFrame(this.#update);
+    }
+
+    #updateProp(name, value) {
+        const type = this.constructor.propTypes[name];
+
+        this.props[name] = type(value);
+    }
+
+    connectedCallback() {
+        this.#update();
+
+        this.componentDidConnect();
+    }
+
+    disconnectedCallback() {
+        patch(this.rootNode, this.#oldTree, null);
+
+        this.componentDidDisconnect();
+    }
+
+    attributeChangedCallback(propName, prevValue, newValue) {
+        if(newValue === prevValue) {
+            return;
+        }
+
+        this.#updateProp(propName, newValue, prevValue);
+
+        this.#requestUpdate();
+    }
+
+    componentDidConnect() {}
+
+    componentDidDisconnect() {}
+
+    render(_state) {}
+
+}
